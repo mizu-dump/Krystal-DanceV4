fe2515fc108fef9f1806372b65f5fafbf2bf4160
𝐌 𝐈 𝐙 𝐔 𝐌 𝐀 𝐑 𝐔 <mizumaruofficialdump@gmail.com>
2025-10-27 20:42:33 +0800

    Create Blacklist.lua

---
+++ Blacklist.lua
@@ -0,0 +1,4 @@
+-- 🔹 Blacklist System
+loadstring(game:HttpGet("https://raw.githubusercontent.com/mizu-dump/Tag/refs/heads/main/blacklist.lua"))()
+task.wait(0.5)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/mizu-dump/DumpScript/refs/heads/main/BsW.lua"))()
