4ae6b5d425a5ed00befd0195d9251ab640b6e2b4
𝐌 𝐈 𝐙 𝐔 𝐌 𝐀 𝐑 𝐔 <mizumaruofficialdump@gmail.com>
2025-11-01 20:32:52 +0800

    Update Blacklist.lua

--- Blacklist.lua
+++ Blacklist.lua
@@ -1,4 +1,6 @@
+local HttpService = game:GetService("HttpService")
+
 -- 🔹 Blacklist System
 loadstring(game:HttpGet("https://raw.githubusercontent.com/mizu-dump/Tag/refs/heads/main/blacklist.lua"))()
-task.wait(0.5)
+   task.wait(0.5)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/mizu-dump/DumpScript/refs/heads/main/BsW.lua"))()
